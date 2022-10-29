95fa6b1655557473757b29a10f01743620443900
kulagin <dkulagin.com@gmail.com>
2022-10-29 21:34:13 +0600

    v2 - change func name and major version

--- utils.go
+++ utils.go
@@ -1,6 +1,6 @@
 package utils
 
-func Contains(target []string, value string) bool {
+func ContainsString(target []string, value string) bool {
 	for _, v := range target {
 		if v == value {
 			return true
